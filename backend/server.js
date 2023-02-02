0f7c41000efd2ccb4dcd52d590c67164fabaca62
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-02 18:07:25 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv'
+import express from 'express'
+import session from 'express-session'
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3050;
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on Port: ${PORT}`)
+})
