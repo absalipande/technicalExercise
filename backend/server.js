84a8350352c89fbcc5e8b5b9010cc6036541b3a9
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-02 18:31:48 +0800

    feat: used express-session to manage user session

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
-import * as dotenv from 'dotenv'
-import express from 'express'
-import session from 'express-session'
-import authRouter from './routes/authRouter.js'
+import * as dotenv from 'dotenv';
+import express from 'express';
+import session from 'express-session';
+import authRouter from './routes/authRouter.js';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3050;
 
-app.use(express.json())
+app.use(express.json());
+app.use(
+  session({
+    secret: 'random-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(authRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is up and running on Port: ${PORT}`)
-})
+  console.log(`Server is up and running on Port: ${PORT}`);
+});
