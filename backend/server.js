28c03318858fe101cafb354403216dccbc0caaea
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-03 21:04:27 +0800

    fix: delete the corsMiddleware

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,14 @@ import cors from 'cors';
 import helmet from 'helmet';
 
 import authRouter from './routes/authRouter.js';
-import { corsMiddleware } from './middleware/corsMiddleware.js';
+// import { corsMiddleware } from './middleware/corsMiddleware.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3050;
 
-app.use(corsMiddleware);
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
