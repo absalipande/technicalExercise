6354d5b1b939c85e51d838d4b32b5a107f885e78
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-03 17:25:59 +0800

    feat: applied useHistory instead in LoginForm

--- frontend/vite-project/src/components/LoginForm.jsx
+++ frontend/vite-project/src/components/LoginForm.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -32,14 +34,14 @@ const LoginForm = () => {
       console.log(response.data);
       setLoading(false);
       localStorage.setItem('token', response.data.token);
-      window.location.href = '/home';
+      history.push('/home');
     } catch (error) {
       console.error(error);
       setError(error.message || 'Something went wrong, please try again later');
       setLoading(false);
     }
   };
-
+  
   return (
     <div className='bg-gray-800 flex flex-col justify-center h-screen'>
       <form
