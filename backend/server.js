30b29131b0d788d72fcb2cd72af6aa1965922887
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-03 11:29:28 +0800

    feat: setup a middleware for CORS

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import session from 'express-session';
 import cors from 'cors';
 import helmet from 'helmet';
 
+import corsMiddleWare from './middleware/corsMiddleware.js';
 import authRouter from './routes/authRouter.js';
 
 dotenv.config();
@@ -11,25 +12,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3050;
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use(corsMiddleWare);
 app.use(helmet());
-
-// app.use(
-//   cors({
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
-//     credentials: true,
-//   })
-// );
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
