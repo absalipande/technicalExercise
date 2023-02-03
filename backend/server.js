87b2fb7edf7dcf2c7c128a015b23c1f0a718cdaa
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-03 11:19:26 +0800

    feat: tried using the cors proxy but to no avail

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,24 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3050;
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use(helmet());
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
