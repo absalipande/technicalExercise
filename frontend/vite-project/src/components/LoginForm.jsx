146317186429a3144c83fb1deecde9e0fa08fd3c
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-03 15:21:09 +0800

    fix: removed unnecessary headers inside the handleSubmit of LoginForm

--- frontend/vite-project/src/components/LoginForm.jsx
+++ frontend/vite-project/src/components/LoginForm.jsx
@@ -21,9 +21,6 @@ const LoginForm = () => {
         {
           headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers':
-              'Origin, X-Requested-With, Content-Type, Accept',
           },
         }
       );
