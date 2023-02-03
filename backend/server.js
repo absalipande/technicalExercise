99016351abb28312d897cbc728c846e75a79cbc3
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-03 09:59:46 +0800

    fix: changed the structure of my server.js

--- backend/server.js
+++ backend/server.js
@@ -3,29 +3,33 @@ import express from 'express';
 import session from 'express-session';
 import cors from 'cors';
 import helmet from 'helmet';
+
 import authRouter from './routes/authRouter.js';
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3050;
 
-const corsOption = {
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  headers: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-app.use(cors(corsOption));
 app.use(helmet());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
     secret: 'random-key',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false },
+    cookie: { secure: false, sameSite: 'strict' },
   })
 );
 app.use(authRouter);
