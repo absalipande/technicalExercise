0397d8a4a55be2a74beede2d9a6d6f167682d7c4
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-03 15:19:08 +0800

    feat: trying some solutions inside the corsMiddleware

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -1,9 +1,9 @@
 export const corsMiddleware = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Origin', ['http://localhost:5173']);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept, X-USER-ID'
   );
   res.header('Access-Control-Allow-Credentials', 'true');
 
